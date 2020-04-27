36344dd7c97f0fe5adceec34c0de642d75f28379
koashima <48111257+koashima@users.noreply.github.com>
2020-04-27 16:47:44 -0700

    added about route

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request): 
     return HttpResponse('<h1>HELLO VINYL COLLECTOR</h1>')
+
+def about(request): 
+    return HttpResponse('<h1>ABOUT VINYL COLLECTOR</h1>')
