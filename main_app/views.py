73ffe9b0b17304e5b518b1ab3530b7f63fdbe137
koashima <48111257+koashima@users.noreply.github.com>
2020-04-27 18:07:24 -0700

    added vinyl index view

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+class Vinyl: 
+    def __init__(self, name, genre, description, r_date):
+        self.name = name
+        self.genre = genre
+        self.description = description
+        self.r_date = r_date
+
+vinyls = [ 
+    Vinyl('Abbey Road', 'rock', 'literally classic', 1969),
+    Vinyl('something1', 'rap', 'literally classic', 2000),
+    Vinyl('2something', 'alternative', 'literally classic', 2001),
+]
+
+
 def home(request): 
     return HttpResponse('<h1>HELLO VINYL COLLECTOR</h1>')
 
 def about(request): 
     return render(request, 'about.html')
+
+def vinyls_index(request):
+    return render(request, 'vinyls/index.html', {'vinyls' : vinyls}) 
